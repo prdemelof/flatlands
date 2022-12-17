a772ebefb034a1e027c81303036251dd77e2554a
paulo de melo <paulo.demelo@hippo-tech.com>
2022-12-17 18:11:32 +0800

    tweak

--- World.js
+++ World.js
@@ -75,7 +75,7 @@ var World = {
 				//TODO: this may spawn mobs in something, like in the ground or another tile
 				var coords_x = System.randomNumberBetween(0, World.map.width) * World.map.tileset.tilewidth;
 				var coords_y = System.randomNumberBetween(0, World.map.height) * World.map.tileset.tileheight;
-				console.log('spawn: ' + mob_id + ' '+coords_x+','+coords_y);
+				//console.log('spawn: ' + mob_id + ' '+coords_x+','+coords_y);
 				World.spawnObject({"type":'mob', "object_id":mob_id, "count":1, "coords": { "x":coords_x, "y": coords_y } });
 			}
 		}
@@ -87,7 +87,7 @@ var World = {
 					World.map.objects['mob'][i].coords.x < 0 || World.map.objects['mob'][i].coords.x + (World.map.objects['mob'][i].image.w * World.map.objects['mob'][i].scale) > (World.map.width * World.map.tileset.tilewidth) ||
 					World.map.objects['mob'][i].coords.y < 0 || World.map.objects['mob'][i].coords.y + (World.map.objects['mob'][i].image.h * World.map.objects['mob'][i].scale) > (World.map.height * World.map.tileset.tileheight)
 				) {
-					console.log('despawn OOB: ' + World.map.objects['mob'][i].id + ' '+World.map.objects['mob'][i].coords.x+','+World.map.objects['mob'][i].coords.y);
+					//console.log('despawn OOB: ' + World.map.objects['mob'][i].id + ' '+World.map.objects['mob'][i].coords.x+','+World.map.objects['mob'][i].coords.y);
 					World.map.objects['mob'].splice(i, 1);
 				}
 			}
